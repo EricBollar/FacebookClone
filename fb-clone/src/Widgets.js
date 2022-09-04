7a43973fac0c2cb6af4968add1c272722aa88ce5
EricBollar <esbollar@gmail.com>
2022-09-04 11:55:10 -0400

    about me links working

--- fb-clone/src/Widgets.js
+++ fb-clone/src/Widgets.js
@@ -10,22 +10,30 @@ function Widgets() {
 	return (
 		<div className="widgets">
 			<h4>About Me</h4>
-			<div className="widgets__option">
-				<h4>My Website</h4>
-				<OpenInNewIcon />
-			</div>
-			<div className="widgets__option">
-				<h4>LinkedIn</h4>
-				<LinkedInIcon />
-			</div>
-			<div className="widgets__option">
-				<h4>GitHub</h4>
-				<GitHubIcon />
-			</div>
-			<div className="widgets__option">
-				<h4>Instagram</h4>
-				<InstagramIcon />
-			</div>
+			<a href="https://www.ericbollar.com" target="_blank">
+				<div className="widgets__option">
+					<h4>My Website</h4>
+					<OpenInNewIcon />
+				</div>
+			</a>
+			<a href="https://www.linkedin.com/in/eric-bollar/" target="_blank">
+				<div className="widgets__option">
+					<h4>LinkedIn</h4>
+					<LinkedInIcon />
+				</div>
+			</a>
+			<a href="https://github.com/EricBollar" target="_blank">
+				<div className="widgets__option">
+					<h4>GitHub</h4>
+					<GitHubIcon />
+				</div>
+			</a>
+			<a href="https://www.instagram.com/ericbollar/" target="_blank">
+				<div className="widgets__option">
+					<h4>Instagram</h4>
+					<InstagramIcon />
+				</div>
+			</a>
 		{/*	<iframe src="https://www.instagram.com/ericbollar/embed" width="340" height="300" frameborder="0" scrolling="yes" allowtransparency="true"></iframe> */}
 		</div>
 	)
